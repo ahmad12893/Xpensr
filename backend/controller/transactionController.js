8c0ec29d8735eb7cd25d63f4d2007af57a351f06
Ahmad Ali <ahmad12893@hotmail.com>
2023-04-22 20:02:53 +0100

    more comments needed, very tired, can filter by days of week, month, year, and custom date

--- backend/controller/transactionController.js
+++ backend/controller/transactionController.js
@@ -21,8 +21,17 @@ const registerTransaction = async (req, res, next) => {
 const fetchTransaction = async (req, res, next) => {
   try {
     const days = parseInt(req.query.days, 10);
-    console.log('days:', days);
-    const fetch = await model.getAll(req.user._id, days);
+    //by default, we will get the first 10 days of transactions, so if there is no query for days, then days will be 10 by default
+    const startDate = req.query.startDate; //this will be the beginning date
+    const endDate = req.query.endDate; //this will be the end date
+    // console.log('days:', days, 'startDate:', startDate, 'endDate:', endDate);
+    const fetch = await model.getAll(
+      req.user._id, //want to get the ID
+      days || null, //if days are given, then get the days
+      startDate || null, //if start date is specified, get the startdate
+      endDate || null // if the end date is specified, get the end date
+    );
+
     res.status(200).json(fetch);
   } catch (error) {
     next(error);
--- backend/model/transactionModel.js
+++ backend/model/transactionModel.js
@@ -1,3 +1,4 @@
+const dayjs = require('dayjs');
 const mongoose = require('./db');
 
 const transactionSchema = new mongoose.Schema({
@@ -15,17 +16,33 @@ const transactionSchema = new mongoose.Schema({
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
 
-const getAll = async (userId, days) => {
-  //make sure getAll accepts days whihc will be subtracted (determined by user on front end)
-  const date = new Date();
-  //show the difference between now and the last however many days e.g. today-7 days as the setDate
-  date.setDate(date.getDate() - days);
+const getAll = async (userId, days, startDate, endDate) => {
+  //create a conditional date, because it may not be normally present
+  let dateCondition;
+  //if a start data and a end date is present, then set the date conditon
+  if (startDate && endDate) {
+    dateCondition = {
+      date: {
+        $gte: dayjs(startDate).toDate(), //gte is mongodb language for greater than or equal to, se this to the start date
+        $lte: dayjs(endDate).toDate(), // this is therefore less than or equal to, so set it to end date ---> need values on the same day or more recent than start, or same date or less recent then end date, in between these 2 dates if they are provided
+      },
+    };
+  } else if (days) {
+    //otherwise, if we are just using the values we put in i.e. '7' or '30' we want to take them away from the current date, so we have all the date greater than current date - number of days
+    const date = new Date();
+    date.setDate(date.getDate() - days); //this will take the current date away from the days we specify when we select in our home page
+    dateCondition = {
+      date: { $gte: date }, //which is why we need to use $gte, and here we get all of the values greater than date = currentdate - days i.e. today is 22/04/2023 - 7 days, all values more recent or on the same day as 15/04/2023 will be shown
+    };
+  } else {
+    dateCondition = {}; //otherwise dont do shit
+  }
 
   const transactions = await Transaction.find({
-    user: userId,
-    date: { $gte: date },
+    user: userId,//still find the user by the user id, so we can isolate transactions by user
+    ...dateCondition,//but push this stupid thing into it too
   });
-  return transactions;
+  return transactions;//get all the transactions back
 };
 
 const createOne = (transaction, userId) => {
